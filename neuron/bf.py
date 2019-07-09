1a482fee2b9fd394bcc4a8f2093d0ba4f0eb9935
Michael Storm <mstorm7@gmail.com>
2019-07-08 22:12:33 -0700

    Allow backward time travel

--- neuron/bf.py
+++ neuron/bf.py
@@ -1,15 +1,27 @@
 from .console import BackgroundColor, TextColor,bold_text, colored_text_background
 from .visitor import TapeIndices
+
+from collections import namedtuple
 from pprint import pprint
+import copy
 import re
 import sys
 
 
+class State:
+    def __init__(self, index, op_start_index, instr_count, number, tape, pointer, output):
+        self.index = index
+        self.op_start_index = op_start_index
+        self.instr_count = instr_count
+        self.number = number
+        self.tape = tape
+        self.pointer = pointer
+        self.output = output
+
+
 class BrainfuckRuntime:
     def __init__(self, declaration_mapper, source, static_data, symbol_table):
-        self.tape = [0] * (TapeIndices.START_STATIC_SEGMENT + 16)
-        self.pointer = 0
-        self.output = ''
+        self.states = []
         self.source = source
         self.static_data = static_data
         self.symbol_table = symbol_table
@@ -17,25 +29,24 @@ class BrainfuckRuntime:
         self.modified_indices = []
         self.breakpoints = []
 
-    def print_source(self, op_start_index):
+    def print_source(self, state):
         source_line_index = None
         for bf_indices, coord in self.symbol_table.items():
-            if coord is not None and op_start_index >= bf_indices[0] and op_start_index < bf_indices[1]:
+            if coord is not None and state.op_start_index >= bf_indices[0] and state.op_start_index < bf_indices[1]:
                 match = re.match(r'.*:(\d+)', coord)
                 if match:
                     source_line_index = int(match.group(1))
 
         for line_index, line in enumerate(self.source.strip().split('\n')):
             if line_index + 1 == source_line_index:
-                print(colored_text_background(BackgroundColor.LIGHT_GREEN, TextColor.BLACK,
-                                              line))
+                print(colored_text_background(BackgroundColor.LIGHT_GREEN, TextColor.BLACK, line))
             else:
                 print(line)
 
-    def print_bf(self, instr_count, op_start_index, op_end_index, code):
+    def print_bf(self, state, code):
         colored_code = ''
 
-        frame_points = [(op_start_index, op_end_index)] + self.breakpoints + [(i, i) for i, c in enumerate(code) if c == '!']
+        frame_points = [(state.op_start_index, state.index)] + self.breakpoints + [(i, i) for i, c in enumerate(code) if c == '!']
         frame_points.sort(key=lambda t: t[0])
 
         end = 0
@@ -48,7 +59,7 @@ class BrainfuckRuntime:
                 index = i + fp[0]
                 is_breakpoint = c == '!' or self.index_in_breakpoint(index)
 
-                if index >= op_start_index and index <= op_end_index:
+                if index >= state.op_start_index and index <= state.index:
                     colored_char = colored_text_background(BackgroundColor.LIGHT_CYAN, TextColor.BLACK, c)
                     if is_breakpoint:
                         colored_char = bold_text(colored_char)
@@ -63,29 +74,29 @@ class BrainfuckRuntime:
 
         colored_code += code[end:]
 
-        code_line_prefix = ' ' * (len(str(instr_count)) + 2)
+        code_line_prefix = ' ' * (len(str(state.instr_count)) + 2)
         code_lines = ('\n' + code_line_prefix).join(colored_code.split('\n'))
-        print('{}: {}'.format(instr_count, code_lines))
+        print('{}: {}'.format(state.instr_count, code_lines))
 
-    def print_tape(self, instr_count):
+    def print_tape(self, state):
         tape_sections = [('', [TapeIndices.START, TapeIndices.END_STOP_INDICATOR]),
                          ('ip', [TapeIndices.START_IP_WORKSPACE, TapeIndices.END_IP_WORKSPACE]),
                          ('stack', [TapeIndices.START_STACK, TapeIndices.END_STACK]),
                          ('lvalues', [TapeIndices.START_LVALUES, TapeIndices.END_LVALUES]),
-                         ('static', [TapeIndices.START_STATIC_SEGMENT, len(self.tape) - 1])]
+                         ('static', [TapeIndices.START_STATIC_SEGMENT, len(self.states[0].tape) - 1])]
 
-        prefix = ' ' * len(str(instr_count)) + '  '
+        prefix = ' ' * len(str(state.instr_count)) + '  '
         colored_tape = ''
         offset = 1
         section_names = []
-        for (value_index, value) in enumerate(self.tape):
+        for (value_index, value) in enumerate(state.tape):
             start_sections = {section[1][0]: section[0] for section in tape_sections}
             if value_index in start_sections:
                 section_names.append([start_sections[value_index], offset])
                 colored_tape += '['
                 offset += 1
 
-            if value_index == self.pointer:
+            if value_index == state.pointer:
                 colored_tape += colored_text_background(BackgroundColor.LIGHT_MAGENTA,
                                                         TextColor.BLACK, value)
             elif value_index in self.modified_indices:
@@ -105,10 +116,10 @@ class BrainfuckRuntime:
         for section_name in section_names:
             section_line = section_line.ljust(section_name[1]) + section_name[0]
 
-        print('{}{}{}'.format(prefix, '({}) '.format(self.pointer).ljust(5), colored_tape))
+        print('{}{}{}'.format(prefix, '({}) '.format(self.states[0].pointer).ljust(5), colored_tape))
         print(prefix + ' ' * 5 + section_line + '\n')
 
-    def print_variables(self):
+    def print_variables(self, state):
         # [0] prevents an empty declaration_position from causing max() to raise error
         max_declaration_length = max([0] + [len(name) for name in self.declaration_mapper.positions])
         reversed_declarations = {value: key for (key, value) in self.declaration_mapper.positions.items()}
@@ -119,65 +130,65 @@ class BrainfuckRuntime:
 
             size = mapped_declaration.declaration.size
             if size == 1:
-                value = self.tape[tape_position]
+                value = self.states[0].tape[tape_position]
             else:
-                value = '{{{}}}'.format(', '.join([str(self.tape[tape_position + 3*i]) for i in range(size)]))
+                value = '{{{}}}'.format(', '.join([str(state.tape[tape_position + 3*i]) for i in range(size)]))
 
             line = '{}{}{}'.format('[{}] '.format(tape_position).rjust(5), padded_name, value)
-            if self.pointer == tape_position:
+            if self.states[0].pointer == tape_position:
                 line = colored_text_background(BackgroundColor.LIGHT_MAGENTA, TextColor.BLACK, line)
             print(line)
 
-    def print_output(self):
-        if len(self.output) > 0:
-            text = colored_text_background(BackgroundColor.RED, TextColor.DEFAULT, self.output)
+    def print_output(self, state):
+        if len(state.output) > 0:
+            text = colored_text_background(BackgroundColor.RED, TextColor.DEFAULT, state.output)
             print(text + '\n')
 
-    def print_state(self, instr_count, op_start_index, op_end_index, code):
-        self.print_source(op_start_index)
+    def print_state(self, state, code):
+        self.print_source(state)
         print()
 
-        self.print_bf(instr_count, op_start_index, op_end_index, code)
+        self.print_bf(state, code)
         print()
 
-        self.print_tape(instr_count)
-        self.print_variables()
+        self.print_tape(state)
+        self.print_variables(state)
         print()
 
-        self.print_output()
+        self.print_output(state)
 
     def get_declaration_value(self, declaration_name):
         position = self.declaration_mapper[declaration_name]
         tape_position = TapeIndices.START_STACK + position
-        return self.tape[tape_position]
+        return self.states[0].tape[tape_position]
 
     def get_array_value(self, declaration_name, offset):
         position = self.declaration_mapper[declaration_name]
         tape_position = TapeIndices.START_STACK + position + offset * 3 + 2
-        return self.tape[tape_position]
+        return self.states[0].tape[tape_position]
 
     def index_in_breakpoint(self, index):
         return any([index >= b_start_index and index <= b_end_index for b_start_index, b_end_index in self.breakpoints])
 
     def execute(self, code, debug=False):
-        index = 0
-        instr_count = 0
         step_into = False
         step_over = False
         step_over_start = None
         prompt_once = False
         comment = False
         color_code = False
-        number = None
-        op_start_index = 0
         previous_input_line = 'c'
         skip_breakpoints = not debug
 
-        while index < len(code):
-            if self.pointer < 0:
-                raise Exception("Bad tape pointer: {}".format(self.pointer))
+        tape = [0] * (TapeIndices.START_STATIC_SEGMENT + 16)
+        state = State(index=0, op_start_index=0, instr_count=0, number=None, tape=tape, pointer=0, output='')
+        self.states.insert(0, state)
 
-            op = code[index]
+        while state.index < len(code):
+            if state.pointer < 0:
+                raise Exception("Bad tape pointer: {}".format(state.pointer))
+
+            op = code[state.index]
 
             if op == '{':
                 comment = True
@@ -190,21 +201,21 @@ class BrainfuckRuntime:
             elif not comment and not color_code:
                 if op == '!' and not skip_breakpoints:
                     step_into = True
-                    op_start_index = index + 1
+                    state.op_start_index = state.index + 1
 
                 elif ord(op) in range(ord('0'), ord('9') + 1):
                     digit = ord(op) - ord('0')
-                    if number is None:
-                        number = digit
+                    if state.number is None:
+                        state.number = digit
                     else:
-                        number = number*10 + digit
+                        state.number = state.number*10 + digit
 
-                elif op in ('+', '-', '>', '<', '.', ',', '[', ']'):
-                    get_input_line = ((step_into or step_over) and step_over_start == None) or self.index_in_breakpoint(index) or prompt_once
+                elif op in '+-><.,[]':
+                    get_input_line = ((step_into or step_over) and step_over_start == None) or self.index_in_breakpoint(state.index) or prompt_once
 
                     if get_input_line:
                         if debug:
-                            self.print_state(instr_count, op_start_index, index, code)
+                            self.print_state(state, code)
 
                         prompt_once = False
                         line = input()
@@ -229,9 +240,20 @@ class BrainfuckRuntime:
                             else:
                                 raise Exception('Accidentally captured command {}'.format(command))
 
+                        elif command == 'S':
+                            if len(self.states) == 1:
+                                print('Reached beginning of state history')
+                            else:
+                                self.states.pop(0)
+                                self.modified_indices = []
+                                state = self.states[0]
+
+                            prompt_once = True
+                            continue
+
                         elif command == 'b':
-                            breakpoint = (op_start_index, index)
-                            if self.index_in_breakpoint(index):
+                            breakpoint = (state.op_start_index, state.index)
+                            if self.index_in_breakpoint(state.index):
                                 self.breakpoints.remove(breakpoint)
                             else:
                                 self.breakpoints.append(breakpoint)
@@ -243,32 +265,37 @@ class BrainfuckRuntime:
                             sys.exit(0)
 
                         else:
+                            print('Unrecognized command "{}"'.format(command))
                             prompt_once = True
                             continue
 
-                    count = 1 if number is None else number
+                    state = copy.deepcopy(state)
+                    self.states.insert(0, state)
+                    self.states = self.states[:1000]
+
+                    count = 1 if state.number is None else state.number
                     for i in range(count):
                         if op == '+':
-                            self.tape[self.pointer] += 1
+                            state.tape[state.pointer] += 1
                             if not get_input_line:
-                                self.modified_indices.append(self.pointer)
+                                self.modified_indices.append(state.pointer)
 
                         elif op == '-':
-                            self.tape[self.pointer] -= 1
+                            state.tape[state.pointer] -= 1
                             if not get_input_line:
-                                self.modified_indices.append(self.pointer)
+                                self.modified_indices.append(state.pointer)
 
                         elif op == '>':
-                            self.pointer += 1
-                            if self.pointer == len(self.tape):
-                                self.tape.append(0)
+                            state.pointer += 1
+                            if state.pointer == len(state.tape):
+                                state.tape.append(0)
 
                         elif op == '<':
-                            self.pointer -= 1
+                            state.pointer -= 1
 
                         elif op == '.':
-                            value = chr(self.tape[self.pointer])
-                            self.output += value
+                            value = chr(state.tape[state.pointer])
+                            state.output += value
 
                         elif op == ',':
                             line = ''
@@ -276,43 +303,43 @@ class BrainfuckRuntime:
                                 print('> ', end='')
                                 line = input()
 
-                            self.tape[self.pointer] = ord(line[0])
+                            state.tape[state.pointer] = ord(line[0])
 
                         elif op == '[':
-                            if self.tape[self.pointer] == 0:
-                                if step_over_start == index:
+                            if state.tape[state.pointer] == 0:
+                                if step_over_start == state.index:
                                     step_over_start = None
 
                                 stack = 1
                                 while stack > 0:
-                                    index += 1
-                                    if code[index] == '[' and code[index-1] != '\033':
+                                    state.index += 1
+                                    if code[state.index] == '[' and code[state.index-1] != '\033':
                                         stack += 1
-                                    elif code[index] == ']':
+                                    elif code[state.index] == ']':
                                         stack -= 1
 
                             elif step_over and step_over_start == None:
-                                step_over_start = index
+                                step_over_start = state.index
 
                         elif op == ']':
                             stack = 1
                             while stack > 0:
-                                index -= 1
-                                if code[index] == '[' and (index == 0 or code[index-1] != '\033'):
+                                state.index -= 1
+                                if code[state.index] == '[' and (state.index == 0 or code[state.index-1] != '\033'):
                                     stack -= 1
-                                elif code[index] == ']':
+                                elif code[state.index] == ']':
                                     stack += 1
 
-                            index -= 1
+                            state.index -= 1
 
-                    instr_count += 1
-                    number = None
-                    op_start_index = index + 1
+                    state.instr_count += 1
+                    state.number = None
+                    state.op_start_index = state.index + 1
 
                 else:
-                    op_start_index = index + 1
+                    state.op_start_index = state.index + 1
 
-            index += 1
+            state.index += 1
 
         if debug:
-            self.print_state(instr_count, op_start_index, index, code)
+            self.print_state(state, code)
